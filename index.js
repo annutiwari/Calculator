7dbd1c9bdf128c440908e918557808f818ec219a
annutiwari <107735048+annutiwari@users.noreply.github.com>
2022-07-20 22:53:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+let outputScreen = document.getElementById("output_screen");
+
+function display(num) {
+  outputScreen.value += num;
+}
+
+function reset() {
+  outputScreen.value = null;
+}
+
+function calculate() {
+  try {
+    outputScreen.value = eval(outputScreen.value);
+  } catch {
+    console.log("error");
+  }
+}
+
+function del() {
+  outputScreen.value = outputScreen.value.slice(0, -1);
+}
 
